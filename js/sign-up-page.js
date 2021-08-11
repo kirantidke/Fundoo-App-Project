548c4e754e75aa07e86a2db2b897db3a3ae85c9c
kirantidke <kirantidke2013@gmail.com>
2021-08-12 01:01:56 +0530

    ADDED:Js file for validat first name i,e valid for only characters

--- js/sign-up-page.js
+++ js/sign-up-page.js
@@ -16,9 +16,17 @@ function validation(){
         document.getElementById('firstname').innerHTML =" ** Please fill the first name field";
         return false;
     }
+    if(!isNaN(firstname)){
+        document.getElementById('firstname').innerHTML =" ** only characters are allowed";
+        return false;
+    }
     
     if(lastname == ""){
         document.getElementById('lastname').innerHTML =" ** Please fill the lastname field";
+        return false;
+    }
+    if(!isNaN(lastname)){
+        document.getElementById('lastname').innerHTML =" ** only characters are allowed";
         return false;
     }
 
@@ -26,7 +34,7 @@ function validation(){
         document.getElementById('username').innerHTML =" ** Please fill the username field";
         return false;
     }
-    /*
+    
     if(username.indexOf('@') <= 0 ){
         document.getElementById('username').innerHTML =" ** @ Invalid Position";
         return false;
@@ -35,28 +43,27 @@ function validation(){
     if((username.charAt(emails.length-4)!='.') && (username.charAt(username.length-3)!='.')){
         document.getElementById('username').innerHTML =" ** . Invalid Position";
         return false;
-    }*/
-    /*
-    if((user.length <= 2) || (user.length > 20)) {
+    }
+    
+    if((username.length <= 2) || (username.length > 20)) {
         document.getElementById('username').innerHTML =" ** Username lenght must be between 2 and 20";
         return false;	
     }
-    if(!isNaN(user)){
+    if(!isNaN(username)){
         document.getElementById('username').innerHTML =" ** only characters are allowed";
         return false;
     }
-    */
+    
    if(pass == ""){
         document.getElementById('password').innerHTML =" ** Please fill the password field";
         return false;
     }
-    /*
+    
     if((pass.length <= 5) || (pass.length > 20)) {
         document.getElementById('password').innerHTML =" ** Passwords lenght must be between  5 and 20";
         return false;	
     }
     
-    */
     if(pass!=confirmpass){
         document.getElementById('password').innerHTML =" ** Password does not match the confirm password";
         return false;
