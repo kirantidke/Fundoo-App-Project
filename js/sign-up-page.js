967ed8b8802d72271b8f78082ab7f7dcfb65b0dc
kirantidke <kirantidke2013@gmail.com>
2021-08-12 01:12:26 +0530

    ADDED:Js file to validate username field i,e invalid without @

--- js/sign-up-page.js
+++ js/sign-up-page.js
@@ -38,7 +38,7 @@ function validation(){
         document.getElementById('username').innerHTML =" ** @ Invalid Position";
         return false;
     }
-    
+    /*
     if((username.charAt(emails.length-4)!='.') && (username.charAt(username.length-3)!='.')){
         document.getElementById('username').innerHTML =" ** . Invalid Position";
         return false;
@@ -52,17 +52,17 @@ function validation(){
         document.getElementById('username').innerHTML =" ** only characters are allowed";
         return false;
     }
-    
+    */
    if(pass == ""){
         document.getElementById('password').innerHTML =" ** Please fill the password field";
         return false;
     }
-    
+    /*
     if((pass.length <= 5) || (pass.length > 20)) {
         document.getElementById('password').innerHTML =" ** Passwords lenght must be between  5 and 20";
         return false;	
     }
-    
+    */
     if(pass!=confirmpass){
         document.getElementById('password').innerHTML =" ** Password does not match the confirm password";
         return false;
